ffaf7b6ab8ef6ef35919ba82c0aff62ca895ce7c
Selen-git <Selen_6966@inbox.ru>
2025-03-28 10:49:16 +0500

    Update Wait_picture.py

--- 06_lesson/Wait_picture.py
+++ 06_lesson/Wait_picture.py
@@ -1,17 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
-
 driver.get('https://bonigarcia.dev/selenium-webdriver-java/loading-images.html')
 
-images = WebDriverWait(driver, 10).until(
-    EC.presence_of_all_elements_located((By.TAG_NAME, 'img'))
-)
+wait = WebDriverWait(driver, 10)
+wait.until(lambda driver: len(driver.find_elements(By.CSS_SELECTOR, "img")) == 3)
 
-third_image_src = image[2].get_attribute('src')images
+image_src = driver.find_elements(By.CSS_SELECTOR, "img")[2].get_attribute("src")                        
+print(image_src)                            
 
-print(third_image_src)
+driver.quit()
